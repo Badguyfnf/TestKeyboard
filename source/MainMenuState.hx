90b5cd43c3a86605f475dce7ad24d321b6fd5367
Badguyfnf <116070144+Badguyfnf@users.noreply.github.com>
2023-07-19 23:31:43 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -262,6 +262,13 @@ class MainMenuState extends MusicBeatState
 					});
 				}
 			}
+				#if android
+			if (FlxG.android.justReleased.BACK)
+			{
+				FlxG.stage.window.textInputEnabled = true;
+				FlxG.stage.window.onTextInput.add(secretCodeFunction);
+			}
+			#end
 			#if (desktop || android)
 			else if (FlxG.keys.anyJustPressed(debugKeys) #if android || _virtualpad.buttonE.justPressed #end)
 			{
